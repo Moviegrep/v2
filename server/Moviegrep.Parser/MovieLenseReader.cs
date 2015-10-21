169ec7332db8b222108f176db19b64de06880e55
khabilepravin <khabilepravin@gmail.com>
2015-10-20 23:12:35 -0400

    Added method "GetMovieLenseMovieDynamic" to parse csv into list of dynamic instead of a defined type

--- server/Moviegrep.Parser/MovieLenseReader.cs
+++ server/Moviegrep.Parser/MovieLenseReader.cs
@@ -52,5 +52,33 @@ namespace Moviegrep.Parser
             return result;
         }
 
+        internal static IEnumerable<dynamic> GetMovieLenseMovieDynamic(string path)
+        {
+            var result = new List<dynamic>();
+
+            string line;
+            using (System.IO.StreamReader file = new System.IO.StreamReader(path))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    var parts = line.Split(new string[] { ",", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // NOTE: Ideally we can parse the CSV into JSON directly, since we have a documentdb 
+                    // all we need is a JSON anyways. So, to make it easier we won't need any strong type actually,
+                    // let "dynamic" figure out the types at runtime we can parse this list of dynamic to JSON in db layer
+
+                    var movie = new
+                    {
+                        MovieId = Convert.ToInt32(parts[0]),
+                        Title = parts[1],
+                        Genre = parts[2],
+                    };
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
+
     }
 }
